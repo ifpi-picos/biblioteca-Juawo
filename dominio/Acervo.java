4bd8fc3517d440d2ec352e82593801d4dda9ef18
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2024-12-14 11:59:44 -0300

    Corrigindo importação em Acervo

--- dominio/Acervo.java
+++ dominio/Acervo.java
@@ -1,12 +1,13 @@
 package dominio;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Acervo {
     private List<Livro> livrosCadastrados;
 
-    public Acervo(List<Livro> livrosCadastrados) {
-        this.livrosCadastrados = livrosCadastrados;
+    public Acervo() {
+        this.livrosCadastrados = new ArrayList<>();
     }
 
     public List<Livro> getLivrosCadastrados() {
@@ -17,7 +18,7 @@ public class Acervo {
         this.livrosCadastrados = livrosCadastrados;
     }
 
-    public void cadastrarLivro(Livro novoLivro){
+    public void cadastrarLivro(Livro novoLivro) {
         System.out.println("Livro cadastrado : " + novoLivro.getTitulo());
         this.livrosCadastrados.add(novoLivro);
     }
