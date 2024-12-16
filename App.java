3a8a96a2a24bf4dd5f7129ec49405029ec65d5b6
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2024-12-16 12:02:19 -0300

    Corrigindo App para implementação da Classe Menu

--- App.java
+++ App.java
@@ -1,12 +1,8 @@
 import dominio.Biblioteca;
-import dominio.Emprestimo;
-import dominio.Livro;
-import dominio.Usuario;
 
 public class App {
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
-        boolean isLoop = true;
-        
+        Menu menu = new Menu(biblioteca);
     }
 }
