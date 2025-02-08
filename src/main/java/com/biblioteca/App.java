47331116d67720824909cc80a410f86c578045ef
João Pedro R. Medeiros de Amorim <joaopedrojp2703@gmail.com>
2025-02-08 19:36:26 -0300

    Adicionando funcionalidades de exibir e pesquisar em LivroDao

--- src/main/java/com/biblioteca/App.java
+++ src/main/java/com/biblioteca/App.java
@@ -1,15 +1,19 @@
 package com.biblioteca;
+
+import com.biblioteca.Dao.ConexaoBD;
 import com.biblioteca.Dao.LivroDao;
 import com.biblioteca.dominio.Biblioteca;
 import com.biblioteca.dominio.Livro;
-import com.biblioteca.dominio.Menu;
-import com.biblioteca.dominio.Usuario;
 
 public class App {
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
-        Livro livro1 = new Livro("livro1", "autor1", "editora1", 2001);
-        Menu menu = new Menu(biblioteca);
-        menu.iniciarMenu();
+        Livro livro1 = new Livro("livro5", "autor1", "editora1", 2001);
+        ConexaoBD conn = new ConexaoBD();
+        LivroDao livroDao = new LivroDao(conn);
+        livroDao.pesquisarLivroTitulo("Senhor dos");
+
+        // Menu menu = new Menu(biblioteca);
+        // menu.iniciarMenu();
     }
 }
